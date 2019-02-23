af58d1545638c4357793789a4f3aa93f7d132c9d
David Espinoza <despinoza1027@gmail.com>
2019-02-23 17:07:41 -0600

    set up random pokemon generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    name: 'bulbasaur',
+    imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
+  }
+
+  randomNumberGenerator() {
+    var randomNumber = Math.ceil(Math.random() * 807);
+    return randomNumber;
+  }
+
+  gottaFetchEmAll() {
+    fetch(`http://pokeapi.co/api/v2/pokemon/${this.randomNumberGenerator()}/`)
+      .then(res => res.json())
+      .then(data => {
+        console.log({ data });
+        this.setState({
+          name: data.name,
+          imageUrl: data.sprites.front_default
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>Randomly generated Pokemon:</div>
+        <div className="Pokemon-Name">{this.state.name}</div>
+        <img className="Sprite" src={this.state.imageUrl} alt="pokemon sprite" />
+        <div>
+          <button className="Button" onClick={() => this.gottaFetchEmAll()}>
+            Click Me
+          </button>
+        </div>
       </div>
     );
   }
